0875b137dcf269a0d49a37d1d2da501bb9c6e338
Tien Minh <minhng130300@gmail.com>
2021-11-20 21:56:11 +0700

    fix 1-M in post models

--- routes/tag.route.js
+++ routes/tag.route.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const router = express.Router();
-
 const Tag = require("../models/tag.model");
 
-router.get("/api/tag", async(req, res, next) => {
-    let tagList = await Tag.find({});
-    res.json({ tag: tagList });
+router.get("/api/tag", async (req, res, next) => {
+  let tagList = await Tag.find({});
+  res.json({ tag: tagList });
 });
 
-router.post("/api/tag", async(req, res, next) => {
-    const { name } = req.body;
-    const newTag = new Tag({ name });
+router.post("/api/tag", async (req, res, next) => {
+  const { name } = req.body;
+  const newTag = new Tag({ name });
 
-    newTag.save((error) => {
-        if (error) {
-          return res.status(500).json({ message: "Sorry, internal server error" });
-        }
-        res.json({
-          message: "Created tag successfully",
-          createTag: newTag
-        });
-      });
+  newTag.save((error) => {
+    if (error) {
+      return res.status(500).json({ message: "Sorry, internal server error" });
+    }
+    res.json({
+      message: "Created tag successfully",
+      createTag: newTag,
+    });
+  });
 });
 
 module.exports = router;
