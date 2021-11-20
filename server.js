2c29587cdbbd0508eb11a8c86a11a1bf10ab4a74
Tien Minh <minhng130300@gmail.com>
2021-11-20 10:13:13 +0700

    get 3 posts from db

--- server.js
+++ server.js
@@ -9,19 +9,17 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "./images");
+    callback(null, "./public/uploads");
   },
   filename: (req, file, callback) => {
-    console.log(file);
     callback(null, Date.now() + path.extname(file.originalname));
   },
 });
 
-const upload = multer({ storage: storage });
-
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(multer({ storage: storage }).single("image"));
 
 mongooose
   .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -30,9 +28,5 @@ mongooose
       console.log("App is runnig");
     });
   });
-
-app.post("/upload", upload.single("image"), (req, res) => {
-  res.json({ imageUrl: req.file.path });
-});
 
 app.use(postRoute);
